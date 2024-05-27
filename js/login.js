c1cde3951fc4c5d12f4b911b9f3ec2919b8bbf86
som <u23533693@tuks.co.za>
2024-05-27 22:15:34 +0200

    fixed the login function

--- js/login.js
+++ js/login.js
@@ -1,50 +1,57 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const submit = document.getElementById('submit');
 
-const  submit = document.getElementById('submit');
+    submit.addEventListener('click', function() { // Add an event listener to the submit button to start process of logging in
+        var email = document.getElementById("login-email");
+        var password = document.getElementById("login-password");
+        var emailValue = email.value;
+        var passwordValue = password.value;
 
-submit.addEventListener('click', function(){//add an event listener to the submit button to start process of loggin in
-	var email = document.getElementById("login-email");
-	var password = document.getElementById("login-password");
-	var emailValue = email.value;
-	var passwordValue = password.value;
-	
-	if(emailValue === ''){
-		alert("Please enter an email address");//validate user input
-		return;
-	}
-	
-	if(passwordValue === ''){
-		alert("Please enter a password");//validate user input
-		return;
-	}
-	
-	login(emailValue,passwordValue);//log user in
-});
-
-function login(email, password){
-    var req = new XMLHttpRequest();
-    req.open("POST", "https://wheatley.cs.up.ac.za/u23584565/COS221/221api.php", true);//send a request to the api
-    var info = [];
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 ){
-                info = JSON.parse(req.responseText);
-                processInfo(info);
+        if (emailValue === '') {
+            alert("Please enter an email address"); // Validate user input
+            return;
         }
-    };
-    var load = JSON.stringify({
-        "action": "Login",
-        "email": email,
-        "password": password
-    });
-    
-    var basicAuth = btoa("u23584565:2023Tukkie2023");
-    req.setRequestHeader("Authorization", "Basic" + basicAuth);
-    req.send(load);
-}
 
-function processInfo(info){
-	if(info.status === "success"){
-		window.location.href = "profile.php";//open page if user credentials correct
-	}else{
-		alert(info.data);////display error message if incorrect credentials provided.
-	}
-}
+        if (passwordValue === '') {
+            alert("Please enter a password"); // Validate user input
+            return;
+        }
+
+        login(emailValue, passwordValue); // Log user in
+    });
+
+    function login(email, password) {
+        var req = new XMLHttpRequest();
+        req.open("POST", "https://wheatley.cs.up.ac.za/u23584565/COS221/221api.php", true); // Send a request to the API
+
+        req.onreadystatechange = function() {
+            if (req.readyState == 4) {
+                if (req.status == 200) {
+                    var info = JSON.parse(req.responseText);
+                    processInfo(info);
+                } else {
+                    alert("Error: " + req.status + " - " + req.statusText);
+                }
+            }
+        };
+
+        var load = JSON.stringify({
+            "action": "Login",
+            "email": email,
+            "password": password
+        });
+
+        var basicAuth = btoa("u23584565:2023Tukkies2023");
+        req.setRequestHeader("Authorization", "Basic " + basicAuth);
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send(load);
+    }
+
+    function processInfo(info) {
+        if (info.status === "success") {
+            window.location.href = "profile.php"; // Open page if user credentials correct
+        } else {
+            alert(info.data); // Display error message if incorrect credentials provided.
+        }
+    }
+});
