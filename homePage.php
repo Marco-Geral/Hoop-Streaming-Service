cd640eb8ac69ac34e87ec1f2b3b6e54d4871b984
som <u23533693@tuks.co.za>
2024-05-28 00:35:54 +0200

    image carousel implemented

--- homePage.php
+++ homePage.php
@@ -13,6 +13,7 @@
 	<link href="css/index.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Overpass&display=swap" rel="stylesheet">
 	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="js/homePage.js"></script>
 
 </head>
 <body>
@@ -29,38 +30,40 @@
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2" class="" aria-current="true"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner">
+<div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/1.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-md-block">
-         <h5 class="text-white-50 release ps-2 fs-6">NEW RELEASES</h5>
-        <h1 class="font_80 mt-4">The Rise Of <br> The Show</h1>
-		<h6 class="text-white"><span class="rating d-inline-block rounded-circle me-2 col_green">6.1</span> <span class="col_green">IMDB SCORE</span> <span class="mx-3">2022</span> <span class="col_red">Romance, Action</span></h6>
-		 <p class="mt-4">Certain but she but shyness why cottage. Guy the put instrument sir entreaties affronting.</p>
-		<h5 class="mb-0 mt-4 text-uppercase"><a class="button" href="#"> Watch Trailer</a></h5>
-      </div>
+        <img src="img/1.jpg" class="d-block w-100" alt="..." style="object-fit: cover; width: 100%; height: 800px;">
+        <div class="carousel-caption d-md-block">
+            <h5 class="text-white-50 release ps-2 fs-6">NEW RELEASES</h5>
+            <h1 class="font_80 mt-4 title">The Rise Of <br> The Show</h1>
+            <h6 class="text-white"><span class="rating d-inline-block rounded-circle me-2 col_green">6.1</span> <span class="col_green">IMDB SCORE</span> <span class="mx-3 year">2022</span> <span class="col_red">Romance, Action</span></h6>
+            <p class="mt-4 des">Certain but she but shyness why cottage. Guy the put instrument sir entreaties affronting.</p>
+            <h5 class="mb-0 mt-4 text-uppercase"><a class="button" href="#"> Watch Trailer</a></h5>
+        </div>
     </div>
     <div class="carousel-item">
-      <img src="img/2.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-md-block">
-         <h5 class="text-white-50 release ps-2 fs-6">NEW RELEASES</h5>
-        <h1 class="font_80 mt-4">Detail Of Popular<br>  New Show</h1>
-		<h6 class="text-white"><span class="rating d-inline-block rounded-circle me-2 col_green">7.3</span> <span class="col_green">IMDB SCORE</span> <span class="mx-3">2022</span> <span class="col_red">Romance, Action</span></h6>
-		 <p class="mt-4">Certain but she but shyness why cottage. Guy the put instrument sir entreaties affronting.</p>
-		<h5 class="mb-0 mt-4 text-uppercase"><a class="button" href="#"> Play Movie</a></h5>
-      </div>
+        <img src="img/2.jpg" class="d-block w-100" alt="..." style="object-fit: cover; width: 100%; height: 800px;">
+        <div class="carousel-caption d-md-block">
+            <h5 class="text-white-50 release ps-2 fs-6">NEW RELEASES</h5>
+            <h1 class="font_80 mt-4 title">Detail Of Popular<br> New Show</h1>
+            <h6 class="text-white"><span class="rating d-inline-block rounded-circle me-2 col_green">7.3</span> <span class="col_green">IMDB SCORE</span> <span class="mx-3 year">2022</span> <span class="col_red">Romance, Action</span></h6>
+            <p class="mt-4 des">Certain but she but shyness why cottage. Guy the put instrument sir entreaties affronting.</p>
+            <h5 class="mb-0 mt-4 text-uppercase"><a class="button" href="#"> Play Movie</a></h5>
+        </div>
     </div>
     <div class="carousel-item">
-      <img src="img/3.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-md-block">
-         <h5 class="text-white-50 release ps-2 fs-6">NEW RELEASES</h5>
-        <h1 class="font_80 mt-4">Lorem Ipsum <br> Sit Dolor</h1>
-		<h6 class="text-white"><span class="rating d-inline-block rounded-circle me-2 col_green">8.2</span> <span class="col_green">IMDB SCORE</span> <span class="mx-3">2022</span> <span class="col_red">Romance, Action</span></h6>
-		 <p class="mt-4">Certain but she but shyness why cottage. Guy the put instrument sir entreaties affronting.</p>
-		<h5 class="mb-0 mt-4 text-uppercase"><a class="button" href="#"> Watch Trailer</a></h5>
-      </div>
+        <img src="img/3.jpg" class="d-block w-100" alt="..." style="object-fit: cover; width: 100%; height: 800px;">
+        <div class="carousel-caption d-md-block">
+            <h5 class="text-white-50 release ps-2 fs-6">NEW RELEASES</h5>
+            <h1 class="font_80 mt-4 title">Lorem Ipsum <br> Sit Dolor</h1>
+            <h6 class="text-white"><span class="rating d-inline-block rounded-circle me-2 col_green">8.2</span> <span class="col_green">IMDB SCORE</span> <span class="mx-3 year">2022</span> <span class="col_red">Romance, Action</span></h6>
+            <p class="mt-4 des">Certain but she but shyness why cottage. Guy the put instrument sir entreaties affronting.</p>
+            <h5 class="mb-0 mt-4 text-uppercase"><a class="button" href="#"> Watch Trailer</a></h5>
+        </div>
     </div>
-  </div>
+</div>
+
+
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
--- js/homePage.js
+++ js/homePage.js
@@ -1,56 +1,55 @@
-function setCarousel (){
+function setCarousel (){// function to make request to api to change image carousel
 	var req = new XMLHttpRequest();
-	req.open("POST", "https://wheatley.cs.up.ac.za/u23584565/COS221/221api.php", true);
+	req.open("POST", "https://wheatley.cs.up.ac.za/u23584565/COS221/221api.php", true);//make the api request
 	var images = [];
 	
 	req.onreadystatechange = function () {
-        if (req.readyState == 4 ){
-		 if(req.status == 200) {
-            images = JSON.parse(req.responseText);
-            processImages(images);
-        }
+    if (req.readyState == 4) {
+        if (req.status == 200) {
+            if (req.responseText) {
+                try {
+                    images = JSON.parse(req.responseText);
+                    processCarousel(images);
+                } catch (error) {
+                    console.error("Error parsing JSON:", error);
+                }
+            } else {
+                console.error("Empty response from server");
+            }
         } else {
-            console.error("Error fetching data");
+            console.error("Error:", req.status);
         }
-    };
+    }
+};
+
     var load = JSON.stringify({
   		"action":"GetAllShows",
-  		"limit":100,
+  		"limit":3,
   		"return":"*"
 	});
-    req.send(load);
+    var basicAuth = btoa("u23584565:2023Tukkies2023");
+    req.setRequestHeader("Authorization", "Basic " + basicAuth);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.send(load);// send request
 }
 
-function processImages () {
+function processCarousel(images) {//function to put data from api into image carousel
+	var display = document.getElementsByClassName("d-block w-100");
+	var names = document.getElementsByClassName("title");
+	var descriptions = document.getElementsByClassName("des");
+	var rating = document.getElementsByClassName("rating");
+	var year = document.getElementsByClassName("year");
 	
+	for(var i = 0; i < display.length; i++) {
+   		display[i].src = images.data[i].imgURL;
+   		names[i].innerText = images.data[i].title; 
+   		descriptions[i].innerText = images.data[i].description; 
+   		rating[i].innerHTML = images.data[i].rating;
+   		year[i].innerHTML = images.data[i].release_date;
+	}
+
 }
 
-function fetchListings() {
-	const loaded = document.getElementById("load");
-    var req = new XMLHttpRequest();
-    req.open("POST", "https://wheatley.cs.up.ac.za/api/", true);//asynchronous so that user can view listings while others are still being rendered.
-    var listings = [];
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 ){
-		loaded.style.display = "none";
-		 if(req.status == 200) {
-            listings = JSON.parse(req.responseText);
-            processListings(listings, loaded);
-        }
-        } else {
-            console.error("Error fetching data");
-        }
-    };
-    var load = JSON.stringify({
-        "studentnum": "u23533693",
-        "type": "GetAllListings",
-        "limit": 100,
-        "apikey": "26b26b692f4b9e415a1e35a02b374944",
-        "return": ["id","title","price","location","bedrooms","bathrooms", "type", "amenities", "description"]
-    });
-	loaded.style.display = "block";
-    req.send(load);
-}
-document.addEventListener("DOMContentLoaded", function(){
-	
-});
+window.onload = function() {
+    setCarousel();
+};
